da8492a9259ec73e802a432f319bc8437f4916c7
SergejsRom <sergejsromanovskis@gmail.com>
2022-06-15 11:20:07 +0300

    cache

--- 024/index.php
+++ 024/index.php
@@ -1,30 +1,55 @@
 <?php
-
-// create curl resource
-$ch = curl_init();
-
-// set url
-curl_setopt($ch, CURLOPT_URL, 'https://www.distance24.org/route.json?stops=Hamburg|Berlin');
-
- //return the transfer as a string
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-  // $output contains the output string
-$output = curl_exec($ch); //siunciam, laukiam, gaunam
-
-// close curl resource to free up system resources
-curl_close($ch); 
-
-$output = json_decode($output);
-
-if($_SERVER['REQUEST_METHOD'] == 'GET')
-?>
-<form action="" method="post">
-    from: <input type="text" name="">
-    from: <input type="text" name="">
-</form>
-
-echo $output->distance;
+require __DIR__ . '/Cache.php';
+session_start();
+$cache = new Cache;
 
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $_SESSION['visual'] = 'CACHE';
+    $output = $cache->get();
+    if (null === $output) {
+        $_SESSION['visual'] = 'LIVE';
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://www.distance24.org/route.json?stops='.$_POST['from'].'|'.$_POST['to']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        
+        $output = curl_exec($ch); // siuncia, laukiam, gaunam
+        
+        curl_close($ch); 
+        
+        $output = json_decode($output);
+        $cache->set($output);
+    }
+
+$_SESSION['dist'] = $output->distance;
+$_SESSION['img1'] = $output->stops[0]->wikipedia->image;
+
+header('Location: http://localhost/bank/024/');
+die;
+
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $dist = $_SESSION['dist'] ?? '';
+    $img1 = $_SESSION['img1'] ?? '';
+    $visual = $_SESSION['visual'] ?? 'dar nera visualo';
+    unset($_SESSION['dist'], $_SESSION['img1'], $_SESSION['visual']);
+    ?>
+    <h1 style="color:red;"><?= $visual ?></h1>
+    <form action="" method="post">
+        from: <input type="text" name="from">
+        to: <input type="text" name="to">
+        <button type="submit">CALC</button>
+    </form>
+    <h2><?= $dist ?></h2>
+    <?php if( $img1): ?>
+
+        <img src="<?= $img1 ?>">
+
+    <?php endif ?>
+    <?php
+}
 
